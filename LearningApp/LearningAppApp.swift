a7a7549c899890dd0c4670e1d5228a7152ad5337
DarrickTruong <55263805+DarrickTruong@users.noreply.github.com>
2021-05-10 15:27:53 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Darrick_Truong on 5/10/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
